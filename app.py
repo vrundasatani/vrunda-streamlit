b89cdd50a4435a168d4aa262b106d6aa9b85f269
vrundasatani <88775697+vrundasatani@users.noreply.github.com>
2023-05-22 23:22:34 +0530

    app.py

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ def main():
        else:
            st.write(f"The severity prediction is slight injury")
         
-       st.write("Developed By: Avi kumar Talaviya")
+       st.write("Developed By: Vrunda_Satani")
        st.markdown("""Reach out to me on: [Twitter](https://twitter.com/avikumart_) |
        [Linkedin](https://www.linkedin.com/in/avi-kumar-talaviya-739153147/) |
        [Kaggle](https://www.kaggle.com/avikumart) 
